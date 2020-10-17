5c5670a36bcc296cad87cd116079d55f4df19cf0
DocG1956 <garybales1029@yahoo.com>
2020-10-17 18:37:41 -0500

    Add project files.

---
+++ FactoryMethod/AirConditioner.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    public class AirConditioner
+    {
+        private readonly Dictionary<Actions, AirConditionerFactory> _factories;
+
+        private AirConditioner()
+        {
+            _factories = new Dictionary<Actions, AirConditionerFactory>();
+
+            foreach (Actions action in Enum.GetValues(typeof(Actions)))
+            {
+                //var actions = Enum.GetValues(typeof(Actions));
+                var name = Enum.GetName(typeof(Actions), action);
+                var instancename = "FactoryMethod." + name + "Factory";
+                var abstractinstance = (AirConditionerFactory)Activator.CreateInstance(Type.GetType(instancename));
+                var exactinstance = Activator.CreateInstance(Type.GetType(instancename));
+
+                var factory = (AirConditionerFactory)Activator.CreateInstance(Type.GetType("FactoryMethod." + Enum.GetName(typeof(Actions), action) + "Factory"));
+                _factories.Add(action, factory);
+            }
+        }
+
+        public static AirConditioner InitializeFactories() => new AirConditioner();
+
+        public IAirConditioner ExecuteCreation(Actions action, double temperature) =>_factories[action].Create(temperature);
+    }
+}
